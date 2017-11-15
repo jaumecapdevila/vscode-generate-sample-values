7dc23c7c80f4af64cd1a63be7c1154d71bfab5ac
Jaume Capdevila Pérez <contact@jaumecapdevila.net>
2017-11-16 00:49:14 +0100

    Now the value is directly printed in the current open editor

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,8 @@ import * as vscode from 'vscode';
 import { Generator } from './generator';
 import { UuidValue } from './values/uuidValue';
 import { window } from 'vscode';
+import { workspace } from 'vscode';
 import { Value } from './values/value';
-import * as copy from 'copy-to-clipboard';
-
-const ncp = require("copy-paste");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,34 +14,37 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.generate', () => {
-        vscode.window.showQuickPick([
-            'uuid',
-            'test'
-        ]).then((geneartor) => {
-            switch (geneartor) {
-                case 'uuid':
-                    const generator = new Generator();
-                    const value: Value = new UuidValue();
-                    const valueGenerated: string = generator.execute(value);
-                    copyGeneratedValue(valueGenerated);
-                    break;
-                default:
-                    window.showErrorMessage("Generator not found");
-                    break;
-            }
-        }, (error) => {
-            window.showErrorMessage("Invalid generator: " + error);
+    let disposable = vscode.commands.registerCommand(
+        'extension.generate',
+        () => {
+            const textEditor = window.activeTextEditor;
+            window.showQuickPick([
+                'uuid',
+                'test'
+            ]).then((geneartor) => {
+                switch (geneartor) {
+                    case 'uuid':
+                        const generator = new Generator();
+                        const value: Value = new UuidValue();
+                        textEditor.edit(function (editor) {
+                            editor.insert(textEditor.selection.active, generator.execute(value))
+                        }).then((done) => {
+                            if (!done) {
+                                window.showErrorMessage("An unexpected error has occurred");        
+                            }
+                        });
+                        break;
+                    default:
+                        window.showErrorMessage("Generator not found");
+                        break;
+                }
+            }, (error) => {
+                window.showErrorMessage("Invalid generator: " + error);
+            });
         });
-    });
     context.subscriptions.push(disposable);
 }
 
-function copyGeneratedValue(valueGenerated: string) {
-    ncp.copy(valueGenerated, function () {
-        window.showInformationMessage("Generated value '" + valueGenerated + "' copied to the clipboard");
-    })
-}
 // this method is called when your extension is deactivated
 export function deactivate() {
     console.log("Vscode value generator extension deactivated");
