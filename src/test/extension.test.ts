959893f37623fe1868b6693cd919d9b8b7511690
Jaume Capdevila Pérez <contact@jaumecapdevila.net>
2017-11-16 01:04:50 +0100

    Added a new case to the test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -10,15 +10,24 @@ import * as assert from 'assert';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import { UuidValue } from '../values/uuidValue';
+import { NameValue } from '../values/nameValue';
+import { Value } from '../values/value';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Uuid generator", () => {
+suite("Generator", () => {
 
     // Defines a Mocha unit test
     test("It should return a valid uuid", () => {
         const uuidPattern: RegExp = /[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/;
-        const value = new UuidValue();
+        const value: Value = new UuidValue();
         const uuid:string = value.generate();
         assert.equal(true, uuidPattern.test(uuid));
+    });
+
+    test("It should return a valid name", () => {
+        const namePattern: RegExp = /(Mr\.|Mrs\.) ([a-z]{16})/;
+        const value: Value = new NameValue();
+        const name: string = value.generate();
+        assert.equal(true, namePattern.test(name));
     });
 });
